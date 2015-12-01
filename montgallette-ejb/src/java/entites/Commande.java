6f67bf9c2d7c9294a5c60160ed66dc87e50d65a2
cdi404 <cdi404@pltz899.AFPA-PARIS-DI>
2015-12-01 13:23:30 +0100

    création Commande (entity)

---
+++ montgallette-ejb/src/java/entites/Commande.java
@@ -0,0 +1,110 @@
+
+package entites;
+
+import java.io.Serializable;
+import java.util.ArrayList;
+import java.util.Date;
+import java.util.HashMap;
+import java.util.List;
+import java.util.Map;
+import javax.persistence.Entity;
+import javax.persistence.GeneratedValue;
+import javax.persistence.GenerationType;
+import javax.persistence.Id;
+import javax.persistence.OneToMany;
+import javax.persistence.Temporal;
+import javax.persistence.TemporalType;
+
+/**
+ *
+ * @author cdi404
+ */
+@Entity
+public class Commande implements Serializable {
+    private static final long serialVersionUID = 1L;
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+
+    @OneToMany (mappedBy="commande")
+    private List produits;   
+    
+    private String numero;
+    // @Temporal(javax.persistence.TemporalType.TIMESTAMP)
+    @Temporal(TemporalType.DATE)
+    private Date date;
+
+    public Commande() {
+        this.produits = new ArrayList();
+    }
+
+    public Commande(Long id, List produits, String numero, Date date) {
+        this.id = id;
+        this.produits = produits;
+        this.numero = numero;
+        this.date = date;
+    }
+
+    public List getProduits() {
+        return produits;
+    }
+
+    public void setProduits(List produits) {
+        this.produits = produits;
+    }
+
+    public String getNumero() {
+        return numero;
+    }
+
+    public void setNumero(String numero) {
+        this.numero = numero;
+    }
+
+    public Date getDate() {
+        return date;
+    }
+
+    public void setDate(Date date) {
+        this.date = date;
+    }
+
+  
+    
+    
+    
+    
+    public Long getId() {
+        return id;
+    }
+
+    public void setId(Long id) {
+        this.id = id;
+    }
+
+    @Override
+    public int hashCode() {
+        int hash = 0;
+        hash += (id != null ? id.hashCode() : 0);
+        return hash;
+    }
+
+    @Override
+    public boolean equals(Object object) {
+        // TODO: Warning - this method won't work in the case the id fields are not set
+        if (!(object instanceof Commande)) {
+            return false;
+        }
+        Commande other = (Commande) object;
+        if ((this.id == null && other.id != null) || (this.id != null && !this.id.equals(other.id))) {
+            return false;
+        }
+        return true;
+    }
+
+    @Override
+    public String toString() {
+        return "entites.Commande  id=" + id;
+    }
+    
+}
