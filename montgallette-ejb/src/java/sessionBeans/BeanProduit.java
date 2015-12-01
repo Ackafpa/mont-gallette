c913a825bf6a72386d536bb4a93b4e844760ea34
cdi411 <cdi411@pltz829.AFPA-PARIS-DI>
2015-12-01 15:26:47 +0100

    Merge branch 'master' into kenTestBranch
    # Conflicts:
    #	src/classLibrairie/Produit.java

--- montgallette-ejb/src/java/sessionBeans/BeanProduit.java
+++ montgallette-ejb/src/java/sessionBeans/BeanProduit.java
@@ -1,10 +1,28 @@
 
 package sessionBeans;
 
+import entites.Produit;
+import java.util.ArrayList;
 import javax.ejb.Stateless;
+import javax.persistence.EntityManager;
+import javax.persistence.PersistenceContext;
 
 @Stateless
 public class BeanProduit implements BeanProduitLocal {
 
+    @PersistenceContext(unitName="montgallette-ejbPU")
+    private EntityManager em;
+    
+    
+    
+    public ArrayList<Produit> consulterMenu(){
+        
+        String req = "select ";
+        ArrayList<Produit> produits = new ArrayList();
+        
+        
+        
+        return produits;
+    }
     
 }
