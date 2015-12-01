2ced83df57618334b4d13a3c69caec70d59d93a2
cdi404 <cdi404@pltz899.AFPA-PARIS-DI>
2015-12-01 14:23:30 +0100

    création Commande (entity)

--- montgallette-ejb/src/java/sessionBeans/BeanCommande.java
+++ montgallette-ejb/src/java/sessionBeans/BeanCommande.java
@@ -1,18 +1,17 @@
 
 package sessionBeans;
 
-import javax.ejb.Stateless;
-import javax.ejb.LocalBean;
 import javax.ejb.Stateful;
 
 /**
  *
  * @author cdi404
- * Il est stateful or stateless ??? 
  */
 @Stateful
-@LocalBean
-public class BeanCommande {
+public class BeanCommande implements BeanCommandeLocal {
 
+ 
+    
+    
     
 }
