a975706e1f65dec50c0f6d9174257f281457cf22
CDI417 <CDI417@pltz902.AFPA-PARIS-DI>
2015-12-08 15:57:49 +0100

    Merge origin/master
    Conflicts:
    	montgallette-ejb/src/java/sessionBeans/BeanTablee.java

--- montgallette-ejb/src/java/sessionBeans/BeanTablee.java
+++ montgallette-ejb/src/java/sessionBeans/BeanTablee.java
@@ -21,8 +21,6 @@ public class BeanTablee implements BeanTableeLocal {
         for (Commande c : t.getCommandes()) {
             //A FINALISER / VERIFIER AVEC CLASSE COMMANDE
             // somme += t.getCommandes().getMontantCommande();
-
-            
         }
         return somme;
     }
