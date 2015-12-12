c610a01328687ce03a1876ad8d6e84760a1a4ba6
oneTeen <kenneth.pino@gmail.com>
2015-12-12 13:58:48 +0100

    new commit

--- montgallette-war/src/java/subcontrollers/Cuisine.java
+++ montgallette-war/src/java/subcontrollers/Cuisine.java
@@ -12,57 +12,6 @@ import javax.servlet.http.HttpServletResponse;
 import sessionBeans.BeanCommandeLocal;
 
 public class Cuisine implements ControllerInterface, Serializable {
-    
-    BeanCommandeLocal beanCommande = lookupBeanCommandeLocal();
-
-    @Override
-    public String execute(HttpServletRequest request, HttpServletResponse response, HttpServlet servlet) {
-        String url = "cuisine.jsp";
-        String action = request.getParameter("action");
-
-        if ("menu".equalsIgnoreCase(action)) {
-            url = "Menu.jsp";
-        }
-        if ("home".equalsIgnoreCase(action)) {
-            url = "login.jsp";
-        }
-
-        return url;
-    }
-
-    private BeanCommandeLocal lookupBeanCommandeLocal() {
-        try {
-            Context c = new InitialContext();
-            return (BeanCommandeLocal) c.lookup("java:global/montgallette/montgallette-ejb/BeanCommande!sessionBeans.BeanCommandeLocal");
-/*
- * To change this license header, choose License Headers in Project Properties.
- * To change this template file, choose Tools | Templates
- * and open the template in the editor.
- */
-package subcontrollers;
-
-import entites.LigneCommande;
-import java.io.Serializable;
-import java.util.ArrayList;
-import java.util.List;
-import java.util.logging.Level;
-import java.util.logging.Logger;
-import javax.naming.Context;
-import javax.naming.InitialContext;
-import javax.naming.NamingException;
-import javax.servlet.ServletContext;
-import javax.servlet.http.HttpServlet;
-import javax.servlet.http.HttpServletRequest;
-import javax.servlet.http.HttpServletResponse;
-import javax.servlet.http.HttpSession;
-import sessionBeans.BeanCommandeLocal;
-import sessionBeans.BeanLigneLocal;
-
-/**
- *
- * @author cdi406
- */
-public class Cuisine implements ControllerInterface, Serializable {
 
     BeanLigneLocal beanLigne = lookupBeanLigneLocal();
     BeanCommandeLocal beanCommande = lookupBeanCommandeLocal();
